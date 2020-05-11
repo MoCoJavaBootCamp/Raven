a50bbf3c02bdaa6ecdd5b4f080e4ade4eca8780f
azdoolittle <drewdoolittle@gmail.com>
2020-05-10 21:39:39 -0400

    greeting method

--- src/Raven.java
+++ src/Raven.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class Raven {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        greeting();
+    }
+
+    public static void greeting() {
+        System.out.println("Good morning, how are you feeling today?");
     }
 }
